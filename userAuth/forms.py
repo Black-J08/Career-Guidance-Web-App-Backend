108ae005bed28c39ccd7eaa7f61a444c5062385a
Nitin Anand <nitin88mehta@gmail.com>
2023-04-14 21:26:17 +0530

    Added login & signup layouts - Added web pages for login & signup - Added static & media files directory in settings.py - Minimum python 3 is required

--- userAuth/forms.py
+++ userAuth/forms.py
@@ -4,11 +4,8 @@ from django.contrib.auth.models import User
 
 
 class RegistrationForm(UserCreationForm):
-    first_name = forms.CharField(required=True)
-    last_name = forms.CharField(required=True)
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name',
-                  'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
