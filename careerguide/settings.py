0e25efd65fdef712207c569254029af2ffd6b969
Nitin Anand <nitin88mehta@gmail.com>
2023-05-10 05:38:46 +0530

    Added dashboard template - Added dashboard url

--- careerguide/settings.py
+++ careerguide/settings.py
@@ -134,5 +134,5 @@ MEDIA_ROOT = (os.path.join(BASE_DIR, 'media/'))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/dashboard'
 LOGOUT_REDIRECT_URL = '/'
