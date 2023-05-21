e6a8e2ed9f4b66c20cb5caa09bc28e60acdf7ae3
Black-J08 <nitin88mehta@gmail.com>
2023-05-21 20:05:15 +0530

    Added delete button in dashboard page to delete assessments

--- assessment/urls.py
+++ assessment/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.index, name="assessment"),
     path("result/", views.result, name="result"),
     path("result/?uuid=<uuid>/", views.result, name="result"),
+    path("delete/", views.delete_assessment, name="delete_assessment"),
+    path('delete/?uuid=<uuid>/', views.delete_assessment, name='delete_assessment'),
 ]
--- assessment/views.py
+++ assessment/views.py
@@ -1,19 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from .models import Assessment
 from .forms import AssessmentForm
-
-
-@login_required(login_url="/login/")
-def result(request):
-    uuid = request.GET.get("uuid")
-    a = Assessment.objects.get(uuid=uuid)
-    data = {
-        "assessment": a.result
-    }
-    return render(request, 'assessment/result.html', data)
 
 
 @login_required(login_url="/login/")
@@ -32,3 +22,21 @@ def index(request):
             return HttpResponse(form.errors)
     else:
         return render(request, 'assessment/index.html')
+
+
+@login_required(login_url="/login/")
+def result(request):
+    uuid = request.GET.get("uuid")
+    a = Assessment.objects.get(uuid=uuid)
+    data = {
+        "assessment": a.result
+    }
+    return render(request, 'assessment/result.html', data)
+
+
+@login_required(login_url="/lohin/")
+def delete_assessment(request):
+    uuid = request.GET.get("uuid")
+    assessment = Assessment.objects.get(uuid=uuid)
+    assessment.delete()
+    return redirect('dashboard')
